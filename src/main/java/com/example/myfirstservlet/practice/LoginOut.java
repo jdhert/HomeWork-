2f1e0954e77847428fed7e088cc350f92e568061
kitri <kkuldong12@gmail.com>
2023-11-14 17:40:17 +0900

    !!!!!@!

--- src/main/java/com/example/myfirstservlet/practice/LoginOut.java
+++ src/main/java/com/example/myfirstservlet/practice/LoginOut.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -14,19 +15,22 @@ import java.util.HashMap;
 public class LoginOut extends HttpServlet {
     HashMap<String, String> Id = new HashMap<>();
     ServletContext sc;
+
+    boolean check;
+
     public void init() {
-        //ServletContext : LifeCycle WAS 시작부터 끝날때까지
+        check =true;
         sc = this.getServletContext();
-        sc.setAttribute("Check", true );
     }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8"); // 한글로 바꿔줌
         PrintWriter out = resp.getWriter();
-        boolean ck = (boolean) sc.getAttribute("Check");
-        if(!ck) {
+
+        if(!check) {
             out.println("로그아웃 되었습니다.");
-            sc.setAttribute("Check", true);
+            this.getServletContext().setAttribute("sessionCheck", req.getSession());
+            check =true;
             out.println("<br>\n" +
                     "<a href=\"loginOut.html\">다시 로그인하기</a>");
         } else {
@@ -42,19 +46,26 @@ public class LoginOut extends HttpServlet {
         Id = (HashMap<String, String>) this.getServletContext().getAttribute("IdData");
         String id = req.getParameter("ID");
         String password = req.getParameter("password");
-        boolean ck = (boolean) this.getServletContext().getAttribute("Check");
 
+        HttpSession session = req.getSession();
+        if(session.isNew()){ // 최초로 생성된 세션이냐?
+            check = true; //
+        }
         if (id == null || password == null)
             out.println("입력값이 올바르지 않습니다.");
-        else if (Id.containsKey(id) && Id.get(id).equals(password) && ck) {
+        else if (Id.containsKey(id) && Id.get(id).equals(password) && check) {
+                check =false;
                 out.println("로그인 성공 했습니다.");
-                this.getServletContext().setAttribute("Check",false);
+//                HttpSession session = req.getSession();
+//                this.getServletContext().setAttribute("sessionCheck", session);
         }
         else if(!Id.containsKey(id))
             out.println("해당 회원 ID는 존재하지 않습니다.");
         else if(!Id.get(id).equals(password))
             out.println("비밀번호가 틀렸습니다.");
-        else out.println("현재 로그인 상태입니다.");
+        else {
+            out.println("현재 로그인 상태입니다.");
+        }
 
     }
 
